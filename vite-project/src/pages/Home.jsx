5eb4a7362d563cd9efe407b53259a9d1aa5b0ec7
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2025-05-03 00:50:26 +0530

    changes made

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,3 +1,12 @@
-export const Home=()=>{
-return <div></div>;
+export const Home= ()=>{
+    return ( 
+    <div className="min-h-screen">
+      {/* Theme Toggle*/}
+      {/*Backgoround Effects */}  
+      {/*NavBar*/}
+      {/*Main Content --consists of Herp Section, Skills Section and all */}
+
+      {/*Footer */}
+    </div>
+    );
 };
