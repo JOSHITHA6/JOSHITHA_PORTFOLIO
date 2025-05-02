f2da3bdfde3093459a841814c3eb5f2ce27dc226
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2025-05-02 22:28:11 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    
+    <BrowserRouter></BrowserRouter>
     </>
   );
 }
